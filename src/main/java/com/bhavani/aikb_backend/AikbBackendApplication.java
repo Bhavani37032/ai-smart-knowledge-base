d684d5bb7b93ad401ee96599a8c88734bf5d8466
Gundu Bhavani <bhavani@Gundus-MacBook-Air.local>
2025-12-18 21:03:13 +0530

    Day 1: Spring Boot setup with security config and health endpoint

---
+++ src/main/java/com/bhavani/aikb_backend/AikbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bhavani.aikb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavani/aikb_backend/AikbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavani.aikb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
